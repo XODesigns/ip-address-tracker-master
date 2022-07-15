778a61d99fb1fc862171acb3a834fe6596ad4407
XODesigns <86083961+XODesigns@users.noreply.github.com>
2022-07-15 09:56:08 +0200

    add first input function

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const form = document.querySelector("form")
+const form = document.querySelector("form");
+// const ipAdress = document.querySelector(".ip");
+// const locationAddress = document.querySelector(".location");
+// const timezone = document.querySelector(".timezone");
+// const isp = document.querySelector(".isp");
+const searchAddress = document.querySelector("input[type='text']");
+const submitButton = document.querySelector("button");
+const results = document.querySelectorAll(".result");
 
-form.addEventListener("click", (evt)=>{
-    evt.preventDefault()
-})
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  if (searchAddress.value !== "") {
+    results[0].innerHTML = searchAddress.value;
+  }
+  searchAddress.value = "";
+});
 
+// Map generator
+let map = L.map("map").setView([51.505, -0.09], 13);
 
-let map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map).openPopup();
+
+// submitButton.addEventListener("click", () => {
+//   // if(submitAddress !== ""){
+//   // }
+// });
