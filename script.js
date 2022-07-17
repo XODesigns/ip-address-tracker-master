d32b41569bb26be1a828a637937e09b87f918444
Xolelani <xolelanimdima@gmail.com>
2022-07-17 21:30:46 +0200

    add final script functionality

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ form.addEventListener("submit", (evt) => {
 
 //Change marker design
 let icon = L.icon({
-  iconUrl: './images/icon-location.svg'
-})
+  iconUrl: "./images/icon-location.svg",
+});
 
 
-//accessing langitude and latitude
 
 // Map generator
-let map = L.map("map").setView([-26.05524, 28.05943], 13);
+let map = L.map("map")
+map.setView([-26.05524, 28.05943], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
@@ -25,52 +25,55 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 
-
-
+//Function that checks if a value has been entered.
 function getData() {
   if (searchAddress.value !== "") {
-
-    geolocation()
-        
+    geolocation();
   }
   searchAddress.value = "";
 }
 
-function geolocation(){
-      let address = searchAddress.value;
-fetch(
-      "https://geo.ipify.org/api/v2/country,city?apiKey=at_GIUwrgS5ZTCn4TruNJyJ8ZNLQqCaw&ipAddress=" +
-        address
-    )
-      .then((response) => {
-        if (response.status >= 200 && response.status <= 299) {
-          return response.json();
-        } else {
-          throw Error(response.statusText);
-        }
-      })
-      .then((data) => {
-        console.log(data);
-        const location = data.location.city;
-        const postalCode = data.location.postalCode
-        const timezone = data.location.timezone;
-        const isp = data.isp;
-        const longitude = data.location.lng
-        const latitude = data.location.lat
+//fetch API function
+function geolocation() {
+  let address = searchAddress.value;
+  fetch(
+    "https://geo.ipify.org/api/v2/country,city?apiKey=at_GIUwrgS5ZTCn4TruNJyJ8ZNLQqCaw&ipAddress=" +
+      address
+  )
+    .then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    })
+    .then((data) => {
+      const location = data.location.city;
+      const postalCode = data.location.postalCode;
+      const timezone = data.location.timezone;
+      const isp = data.isp;
+      const longitude = data.location.lng;
+      const latitude = data.location.lat;
 
-        L.marker([latitude, longitude], {icon:icon}).addTo(map).openPopup();
-        results[0].innerHTML = address;
-        results[1].innerHTML = `${location} ${postalCode}`;
-        results[2].innerHTML = `UTC${timezone}`;
-        results[3].innerHTML = isp;
+      L.marker([latitude, longitude], { icon: icon }).addTo(map).openPopup();
+       map.setView([latitude, longitude], 13);
+      results[0].innerHTML = address;
+      results[1].innerHTML = `${location} ${postalCode}`;
+      results[2].innerHTML = `UTC${timezone}`;
+      results[3].innerHTML = isp;
+    
+    })
+    .catch(() => {
+      alerts();
+      results[0].innerHTML = "";
+    });
+}
 
-        //adding dynamic map.
-
-
-      })
-      .catch(() => {
-        alert("Enter a correct ipv4 or ipv6 address");
-        results[0].innerHTML = ""
-      });
-
+function alerts(){
+  const alertPopup = document.querySelector(".alert")
+  const alertButton = document.querySelector("[data-alert]")
+  alertPopup.classList.remove("hidden")
+  alertButton.addEventListener("click", ()=>{
+    alertPopup.classList.add("hidden")
+  })
 }
